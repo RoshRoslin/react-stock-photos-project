e6d9770765a67705759385dea764e45cd88929e3
RoshRoslin <RoshRoslin@gmail.com>
2021-05-16 16:49:05 -0700

    Updates

---
+++ precache-manifest.a0cd9333be54b4501ab629b726395276.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbcdf0715a5050f2e9de4070a37b80a4",
+    "url": "/react-/index.html"
+  },
+  {
+    "revision": "235c08e954eca3a804e2",
+    "url": "/react-/static/css/main.a44d4aa5.chunk.css"
+  },
+  {
+    "revision": "b5a99b81e43d98e30c35",
+    "url": "/react-/static/js/2.ee72bb2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-/static/js/2.ee72bb2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "235c08e954eca3a804e2",
+    "url": "/react-/static/js/main.301940ac.chunk.js"
+  },
+  {
+    "revision": "cfa9a6bf2dbb501b7837",
+    "url": "/react-/static/js/runtime-main.cc8a6b5f.js"
+  }
+]);
